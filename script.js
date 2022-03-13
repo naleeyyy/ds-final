7f9f11ebb04454b1e48c5fa2d03f7d0a96985392
naleeyyy <kahmeti123@gmail.com>
2022-03-13 18:57:25 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,80 @@
+const scene = new THREE.Scene();
+let aspectRatio = window.innerWidth / window.innerHeight
+const camera = new THREE.PerspectiveCamera( 75, aspectRatio, 0.1, 1000 )
+const canvas = document.querySelector('#c')
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas,
+    antialias: true,
+})
+
+const main = document.querySelector('main')
+
+renderer.setPixelRatio( window.devicePixelRatio )
+renderer.setSize( window.innerWidth, window.innerHeight ) 
+
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap; 
+
+window.addEventListener('resize', () => {
+    let width = window.innerWidth
+    let height = window.innerHeight
+    renderer.setSize( width, height )
+    camera.aspect = width / height
+    camera.updateProjectionMatrix()
+})
+
+const geometry = new THREE.TorusKnotGeometry( 10, 3, 100, 16 );
+const material = new THREE.MeshPhongMaterial({ wireframe: true })
+const obj = new THREE.Mesh( geometry, material );
+
+obj.castShadow = true
+obj.receiveShadow = true
+
+scene.add( obj );
+
+const lightcolor = 0xffffff
+const light = new THREE.PointLight(lightcolor, 1)
+light.position.set(0, 0, 30)
+light.castShadow = true; 
+scene.add( light )
+
+light.shadow.mapSize.width = 512; 
+light.shadow.mapSize.height = 512;
+light.shadow.camera.near = 1;
+light.shadow.camera.far = 500;
+
+const planeGeometry = new THREE.PlaneGeometry(100, 100);
+const planeMaterial = new THREE.MeshStandardMaterial( { color: 0x000000 } )
+const plane = new THREE.Mesh( planeGeometry, planeMaterial );
+plane.receiveShadow = true;
+scene.add( plane );
+
+const lighthelper = new THREE.PointLightHelper( light )
+// scene.add (lighthelper)
+
+camera.position.z = 25;
+
+let mouseX = 0;
+let mouseY = 0;
+    
+document.onmousemove = (event) => {
+    mouseX = (event.pageX / window.innerWidth - 0.5) * 4;
+    mouseY = ((event.pageY / window.innerHeight - 0.5) * (-1)) * 4;
+}
+
+function animate() {
+	requestAnimationFrame( animate );
+    
+    light.position.set(mouseX * 2, mouseY , 30)
+    
+    camera.position.x = mouseX / -2;
+    camera.position.y = mouseY / -2;
+
+    obj.rotateX(0.005);
+    obj.rotateY(0.005);
+    obj.rotateZ(0.005);
+
+ 	renderer.render( scene, camera );
+}
+
+animate();
---
+++ typed.js
@@ -0,0 +1,10 @@
+const typed = new Typed('#main-header', {
+    strings: [
+        'string 1',
+        'string 2',
+    ],
+    typeSpeed: 100,
+    backSpeed: 100,
+    smartBackspace: false,
+    loop: true,
+})
